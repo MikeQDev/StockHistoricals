b4a49455237afef891f3fdc48cb2d2dfabd209f7
mike <dimattiami>
2017-01-11 11:03:28 -0500

    Add exit statement for invalid args amt

--- 1-symbolRetrieve.py
+++ 1-symbolRetrieve.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 import csv
+import sys
 
-with open('companylist.csv','rb') as csvfile:
+if len(sys.argv) != 2:
+  print "usage: python "+sys.argv[0]+" inCompanyListFile.csv"
+  exit()
+else:
+  print sys.argv[1]
+
+
+with open(sys.argv[1],'rb') as csvfile:
   reader = csv.reader(csvfile)
   for row in reader:
     print row[0]
