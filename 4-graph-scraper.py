f41922a557106bd05ac64ad257d244c1e785ea56
mike <dimattiami>
2017-01-11 11:08:13 -0500

    Update

---
+++ 4-graph-scraper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import csv
+import sys
+import urllib
+
+if len(sys.argv) != 2:
+  print "usage: python "+sys.argv[0]+" analyzed-ratio-sorted.csv"
+  exit()
+else:
+  print sys.argv[1]
+
+
+#count lines, so we know the last ones to pull..
+amount = 0
+with open(sys.argv[1],'rb') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader:
+    amount += 1
+ 
+
+_BASE_URL_ = "https://www.google.com/finance/getchart?x=NASD&p=40Y&i=604800&ei=J092WJCaO4HRjAH27rbYBA&q="
+i = 0;
+with open(sys.argv[1],'rb') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader:
+    i = i+1
+    if i == 1:
+      continue
+    elif i > 50 and i < (amount-50):
+      continue
+
+    print "status: grabbing "+str(i)
+    ticker = row[0][1:-1]
+    ratio = row[1] 
+    urllib.urlretrieve(_BASE_URL_+ticker, "graphs/"+ratio+"-"+ticker+".png")
